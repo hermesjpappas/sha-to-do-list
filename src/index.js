26f1667060651c210a85c9d3c42b83654c5bdff4
Hermes J. Pappas <hjp@hjp.gr>
2021-12-01 15:35:21 +0200

    Make basic rendering

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 import { toDo } from './toDo';
 import { project } from './project';
+import { render } from './render';
 
 
 //until we make it work with localStorage
 let projectArray = [];
 
 
-let test = new toDo("Make this project", "blah blah", "2/12/2021", 3);
+let test = new toDo("Make this project", "Try not to have bugs you stupid idiot", "2/12/2021", 3);
 
 console.log(test.id);
 
-let test2 = new toDo("blah blah", "blah", "3/01/2022", 1);
+let test2 = new toDo("Make more shit", "Render everything", "3/01/2022", 1);
 
 console.log(test2.id);
 
 
 let defaultProject = new project("Default", "#fff");
-projectArray.push(defaultProject);
 
 defaultProject.addToDo(test);
 defaultProject.addToDo(test2);
 
-console.log(defaultProject.list);
 
-defaultProject.removeToDo("T0");
+projectArray.push(defaultProject);
+console.log(projectArray);
 
-console.log(defaultProject.list);
+
+render(projectArray);
+
---
+++ src/render.js
@@ -0,0 +1,45 @@
+import { project } from './project';
+import { toDo } from './toDo';
+
+
+
+function render(projectList) {
+
+   const dash = document.getElementById("dashboard");
+
+    for(let i=0; i<projectList.length; i++) {
+        let proj = projectList[i];
+        console.log(proj);
+        console.log("I am the project list", proj.list);
+
+        let projDiv = document.createElement("div");
+        projDiv.classList.add("project");
+        projDiv.id = proj.id;
+
+        for(let j=0; j<proj.list.length; j++) {
+            
+            let toDoItem = proj.list[j];
+
+            let toDoDiv = document.createElement("div");
+            toDoDiv.id = toDoItem.id;
+            toDoDiv.classList.add("to-do");
+            
+            let title = document.createElement("h1");
+            title.textContent = toDoItem.title;
+
+            let description = document.createElement("p");
+            description.textContent = toDoItem.description;
+
+            toDoDiv.appendChild(title);
+            toDoDiv.appendChild(description);
+
+            projDiv.appendChild(toDoDiv);
+        
+        
+        }
+
+        dash.appendChild(projDiv);
+    }
+}
+
+export { render };
