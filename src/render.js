0e6c1c9b833f383b59a1aa440b829d83be77d4f0
Hermes J. Pappas <hjp@hjp.gr>
2021-12-02 12:43:08 +0200

    Restyle and re-render form

--- src/render.js
+++ src/render.js
@@ -1,116 +1,112 @@
-import { project } from './project';
-import { toDo } from './toDo';
-
-
+import { project } from "./project";
+import { toDo } from "./toDo";
 
 function render(projectList) {
-   
-   const dash = document.getElementById("dashboard");
-   dash.innerHTML = "";
+  const dash = document.getElementById("dashboard");
+  dash.innerHTML = "";
 
-    for(let i=0; i<projectList.length; i++) {
-        let proj = projectList[i];
+  for (let i = 0; i < projectList.length; i++) {
+    let proj = projectList[i];
 
-        let projDiv = document.createElement("div");
-        projDiv.classList.add("project");
-        projDiv.id = proj.id;
+    let projDiv = document.createElement("div");
+    projDiv.classList.add("project");
+    projDiv.id = proj.id;
 
-        let projTitle = document.createElement("h1");
-        projTitle.textContent = proj.title;
-        projDiv.appendChild(projTitle);
+    let projTitle = document.createElement("h1");
+    projTitle.textContent = proj.title;
+    projDiv.appendChild(projTitle);
 
-        let removeButton = document.createElement("div");
-        removeButton.classList.add("remove-proj");
-        removeButton.textContent = "-";
-        projDiv.appendChild(removeButton);
+    let removeButton = document.createElement("div");
+    removeButton.classList.add("remove-proj");
+    removeButton.textContent = "-";
+    projDiv.appendChild(removeButton);
 
-        for(let j=0; j<proj.list.length; j++) {
-            
-            let toDoItem = proj.list[j];
+    for (let j = 0; j < proj.list.length; j++) {
+      let toDoItem = proj.list[j];
 
-            let toDoDiv = document.createElement("div");
-            toDoDiv.id = toDoItem.id;
-            toDoDiv.classList.add("to-do");
-            
-            let title = document.createElement("h1");
-            title.textContent = toDoItem.title;
+      let toDoDiv = document.createElement("div");
+      toDoDiv.id = toDoItem.id;
+      toDoDiv.classList.add("to-do");
 
-            let description = document.createElement("p");
-            description.textContent = toDoItem.description;
+      let title = document.createElement("h1");
+      title.textContent = toDoItem.title;
 
-            let remToDoButton = document.createElement("div");
-            remToDoButton.classList.add("rem-to-do");
-            remToDoButton.textContent = "-";
+      let description = document.createElement("p");
+      description.textContent = toDoItem.description;
 
-            let checkButton = document.createElement("div");
-            checkButton.classList.add("check-to-do");
+      let remToDoButton = document.createElement("div");
+      remToDoButton.classList.add("rem-to-do");
+      remToDoButton.textContent = "-";
 
-            if(toDoItem.done === true) {
-                checkButton.innerHTML = "&#10004;";
-                checkButton.style.backgroundColor = "green";
-                title.style.textDecoration = "line-through";
-                description.style.textDecoration = "line-through";
-                toDoDiv.style.backgroundColor = "rgba(var(--to-do-color), 0.6)";
-            }
-            else {
-                checkButton.innerHTML = "&times;"; 
-                checkButton.style.backgroundColor = "#777";
-            }
-            
+      let checkButton = document.createElement("div");
+      checkButton.classList.add("check-to-do");
 
-            toDoDiv.appendChild(title);
-            toDoDiv.appendChild(description);
-            toDoDiv.appendChild(remToDoButton);
-            toDoDiv.appendChild(checkButton);
+      if (toDoItem.done === true) {
+        checkButton.innerHTML = "&#10004;";
+        checkButton.style.backgroundColor = "green";
+        title.style.textDecoration = "line-through";
+        description.style.textDecoration = "line-through";
+        toDoDiv.style.backgroundColor = "rgba(var(--to-do-color), 0.6)";
+      } else {
+        checkButton.innerHTML = "&times;";
+        checkButton.style.backgroundColor = "#777";
+      }
 
-            projDiv.appendChild(toDoDiv);
-        
-        
-        }
+      toDoDiv.appendChild(title);
+      toDoDiv.appendChild(description);
+      toDoDiv.appendChild(remToDoButton);
+      toDoDiv.appendChild(checkButton);
 
-        let toDoForm = document.createElement("form");
-        toDoForm.id = `to-do-form-${proj.id}`;
-        toDoForm.innerHTML = 
-        
-        `
-            <h1>Add To-Do</h1>
-            <div>
-                <label for="toDoTitle">Title: </label>
-                <input type="text" id="toDoTitle-${proj.id}">
+      projDiv.appendChild(toDoDiv);
+    }
+
+    let toDoCont = document.createElement("div");  
+    toDoCont.id = `to-do-cont-${proj.id}`;
+    toDoCont.classList.add("form-container");
+    toDoCont.innerHTML = `
+    <h1>Add To-Do</h1>
+    <form id="test-form">
+        <div>
+            <label class="l" for="toDoTitle">Title: </label>
+            <input class="r" type="text" id="toDoTitle">
+        </div>
+        <div>
+            <label class="l" for="toDoDetails">Title: </label>
+            <input class="r" ype="text" class="details" id="toDoDetails">
+        </div>
+        <div>
+            <label class="l" for="toDoDate">Date: </label>
+            <input class="r" type="date">
+        </div>
+        <div>
+            <label class="l">Priority: </label>
+            <div class="r">
+                <input type="radio" id="low" name="priority" value="low">
+                <label for="low">Low</label>
+                <input type="radio" id="medium" name="priority" value="medium">
+                <label for="medium">Medium</label>
+                <input type="radio" id="high" name="priority" value="high">
+                <label for="high">High</label>
             </div>
-            <div>
-                <label for="toDoDetails">Title: </label>
-                <input type="text" class="details" id="toDoDetails-${proj.id}">
-            </div>
-            <div>
-                <label for="toDoDate">Dates: </label>
-                <input type="date" id="toDoDate-${proj.id}>
-            </div>
-            <div>
-                <label for="low-${proj.id}">Priority: </label>
-                <input type="radio" id="low-${proj.id}" name="priority" value="low">
-                <label for="low-${proj.id}">Low</label>
-                <input type="radio" id="medium-${proj.id}" name="priority" value="medium">
-                <label for="medium-${proj.id}">Medium</label>
-                <input type="radio" id="high-${proj.id}" name="priority" value="high">
-                <label for="high-${proj.id}">High</label>
-            </div>
-            <div>
-                <button class="td-cancel" id="to-do-cancel-${proj.id}" type="button">Cancel</button>
-                <button class="td-add" id="to-do-add-${proj.id}">Add To-Do</button>
-            </div> 
+        </div>
+
+    </form>
+    <div class="to-do-button-cont">
+        <button class="td-cancel" id="to-do-cancel" type="button">Cancel</button>
+        <button class="td-add" id="to-do-add">Add To-Do</button>
+    </div>
         `;
 
-        projDiv.appendChild(toDoForm);
+    projDiv.appendChild(toDoCont);
 
-        let projAddToDoButton = document.createElement("div");
-        projAddToDoButton.classList.add("proj-to-do");
-        projAddToDoButton.textContent = "+";
+    let projAddToDoButton = document.createElement("div");
+    projAddToDoButton.classList.add("proj-to-do");
+    projAddToDoButton.textContent = "+";
 
-        projDiv.appendChild(projAddToDoButton);
+    projDiv.appendChild(projAddToDoButton);
 
-        dash.appendChild(projDiv);
-    }
+    dash.appendChild(projDiv);
+  }
 }
 
 export { render };
