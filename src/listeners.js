264e6f5225fa403d15d2ae5a5fd6237cdc6af241
Hermes J. Pappas <hjp@hjp.gr>
2021-12-02 13:13:09 +0200

    Make to-do form pop up work and move on to making form buttons work

--- src/listeners.js
+++ src/listeners.js
@@ -11,14 +11,18 @@ const dash = document.getElementById("dashboard");
 
 
 function buttonListeners() {
+    
+    //make new project add pop up appear
     addButton.addEventListener("click", (e) => {
         popUp.style.display = "flex";
     });
 
+    //cancel adding a new project
     cancelButton.addEventListener("click", (e) => {
         popUp.style.display = "none";
     });
 
+    //adding a new project from the form
     formAddButton.addEventListener("click", (e) => {
         e.preventDefault();
         if(!projTitle.value) return;
@@ -30,6 +34,7 @@ function buttonListeners() {
         render(projectArray);
     });
 
+    //event listener for removing projects
     dash.addEventListener("click", (e) => {
         if(e.target.classList.contains("remove-proj")) {
             let id = e.target.parentElement.id;
@@ -40,6 +45,7 @@ function buttonListeners() {
         }
     });
 
+    //event listener for removing individual to-dos
     dash.addEventListener("click", (e) => {
         if(e.target.classList.contains("rem-to-do")) {
             let toDoId = e.target.parentElement.id;
@@ -51,6 +57,7 @@ function buttonListeners() {
         }
     });
 
+    //event listener for each "done" button for to-dos
     dash.addEventListener("click", (e) => {
         if(e.target.classList.contains("check-to-do")) {
             let toDoId = e.target.parentElement.id;
@@ -63,9 +70,22 @@ function buttonListeners() {
         }
     });
 
+
+    //event listener for "add to do" button for each project
     dash.addEventListener("click", (e) => {
         if(e.target.classList.contains("proj-to-do")) {
-          //make form appear 
+            let projId = e.target.parentElement.id;
+            let formName = "to-do-cont-" + projId;
+            let form = document.getElementById(formName);
+            form.style.display = "flex";
+        }
+    });
+
+    //event listener for adding new to-do within project
+    dash.addEventListener("click", (e) => {
+        if(e.target.classList.contains("td-add")) {
+            let projId = e.target.parentElement.parentElement.parentElement.id;
+            console.log(projId);
         }
     });
 }
