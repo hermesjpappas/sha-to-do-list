4700c04d16f7d0b5a134546cca38e84efcbefc30
Hermes J. Pappas <hjp@hjp.gr>
2021-12-03 18:32:31 +0200

    Remove extra test project

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ let projectArray = [];
 
 
 let test = new toDo("Make this project", "Try not to have bugs and to implement more features.", "2021-12-02", "high");
-let test2 = new toDo("Happy birthday!", "Must celebrate.", "2022-09-22", "high");
+let test2 = new toDo("Happy birthday!", "Must celebrate.", "2022-09-22", "medium");
 
 let defaultProject = new project("Default");
 
@@ -17,17 +17,6 @@ defaultProject.addToDo(test);
 defaultProject.addToDo(test2);
 
 projectArray.push(defaultProject);
-
-let test3 = new toDo("Make other project", "Try not to blah", "2023-03-11", "low");
-let test4 = new toDo("Make other other project", "Something here", "2021-12-05", "high");
-let test5 = new toDo("something", "something more", "2021-12-07", "medium");
-
-let importantProject = new project("Important");
-
-importantProject.addToDo(test3);
-importantProject.addToDo(test4);
-importantProject.addToDo(test5);
-projectArray.push(importantProject);
 
 
 render(projectArray);
